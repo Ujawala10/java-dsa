033d0a158f0c0ae26ebc01dff20836ecb1b3284d
Ujawala10 <ujawalasinha@gmail.com>
2026-05-28 23:28:50 +0530

    Added Consecutive

---
+++ Array/Maximum.java
@@ -0,0 +1,24 @@
+package Array;
+
+public class Maximum {
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 0, 1, 1, 1, 0, 1};
+        
+        int count = 0;
+        int max = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == 1){
+                count = count + 1;
+            } else {
+                count = 0;
+            }
+
+            if(count > max){
+                max = count;
+            }
+        }
+
+        System.out.println("Max Consecutive Ones = " + max);
+    }
+}
